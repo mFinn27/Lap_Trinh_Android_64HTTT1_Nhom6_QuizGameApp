4dca05d70790c76b493a02721317f476f89a223a
Thinh <lequocthinh0809@gmail.com>
2025-06-20 23:57:18 +0700

    Tạo tk dành cho user

--- app/src/main/java/com/example/quizapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/quizapp/activity/LoginActivity.java
@@ -1,34 +1,34 @@
-    package com.example.quizapp.activity;
+package com.example.quizapp.activity;
 
-    import android.content.Intent;
-    import android.os.Bundle;
-    import android.view.View;
-    import android.widget.Button;
-    import android.widget.ImageButton;
-    import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-    import androidx.activity.EdgeToEdge;
-    import androidx.annotation.NonNull;
-    import androidx.appcompat.app.AppCompatActivity;
-    import androidx.core.graphics.Insets;
-    import androidx.core.view.ViewCompat;
-    import androidx.core.view.WindowInsetsCompat;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-    import com.example.quizapp.R;
-    import com.google.android.material.textfield.TextInputEditText;
-    import com.google.firebase.auth.FirebaseAuth;
-    import com.google.firebase.auth.FirebaseUser;
-    import com.google.firebase.database.DataSnapshot;
-    import com.google.firebase.database.DatabaseError;
-    import com.google.firebase.database.DatabaseReference;
-    import com.google.firebase.database.FirebaseDatabase;
-    import com.google.firebase.database.ValueEventListener;
+import com.example.quizapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    public class LoginActivity extends AppCompatActivity {
-        private TextInputEditText etEmail, etPassword;
-        Button btnLogin, btnSignup;
-        private FirebaseAuth auth;
-        private ImageButton btn_back;
+public class LoginActivity extends AppCompatActivity {
+    private TextInputEditText etEmail, etPassword;
+    Button btnLogin, btnSignup;
+    private FirebaseAuth auth;
+    private ImageButton btn_back;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
