69bc79b078d1cf308d9e5d155bf9bf591e1815ae
Thinh <lequocthinh0809@gmail.com>
2025-06-20 20:38:05 +0700

    khai bao auth va btl back

--- app/src/main/java/com/example/quizapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/quizapp/activity/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.quizapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +14,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quizapp.R;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private TextInputEditText etEmail, etPassword;
     Button btnLogin, btnSignup;
-
+    private FirebaseAuth auth;
+    private ImageButton btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +31,11 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+        btnSignup = findViewById(R.id.btn_signup);
+        btn_back = findViewById(R.id.button_back);
+        auth = FirebaseAuth.getInstance();
     }
 }
