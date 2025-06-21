387f23b3156210250b651995c934abebf29845cf
mFinn27 <minhtranvan1904@gmail.com>
2025-06-21 21:43:37 +0700

    add event click - adapter update

--- app/src/main/java/com/example/quizapp/adapter/TopicAdapter.java
+++ app/src/main/java/com/example/quizapp/adapter/TopicAdapter.java
@@ -1,5 +1,7 @@
 package com.example.quizapp.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.quizapp.R;
+import com.example.quizapp.activity.AddQuestionActivity;
 import com.example.quizapp.model.Topic;
 
 import java.util.List;
@@ -18,9 +21,11 @@ import java.util.List;
 public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHolder> {
 
     private List<Topic> topics;
+    private Context context;
 
     public TopicAdapter(List<Topic> topics) {
         this.topics = topics;
+        this.context = context;
     }
 
     @NonNull
@@ -34,6 +39,12 @@ public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHol
     public void onBindViewHolder(@NonNull TopicViewHolder holder, int position) {
         Topic topic = topics.get(position);
         holder.tvTopicName.setText(topic.getName());
+
+        holder.btnAdd.setOnClickListener(v -> {
+            Intent intent = new Intent(context, AddQuestionActivity.class);
+            intent.putExtra("topicId", topics.get(position).getId());
+            context.startActivity(intent);
+        });
     }
 
 
@@ -45,11 +56,13 @@ public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHol
     public static class TopicViewHolder extends RecyclerView.ViewHolder {
         ImageView ivTopicIcon;
         TextView tvTopicName;
+        ImageButton btnAdd;
 
         public TopicViewHolder(@NonNull View itemView) {
             super(itemView);
             ivTopicIcon = itemView.findViewById(R.id.iv_topic_icon);
             tvTopicName = itemView.findViewById(R.id.tv_topic_name);
+            btnAdd = itemView.findViewById(R.id.btn_add_question);
         }
     }
 }
